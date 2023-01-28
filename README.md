7569397a26322c4dedbdeffa3d24d6d83e5e1d49
gogocons <gogocons@gmail.com>
2023-01-27 22:53:46 -0600

    misc

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
 # gogoborn
 
 This is my game for Code Class.
@@ -10,12 +12,10 @@ In this game you will be able to choose from 3 characters:
 - Assassin (Marksman)
 - Mage (Destruction)
 
-## In Progress
+### In Progress
 
 You will be able to choose attacks based on their style of combat.
 
 There will be various creature and humanoid enemies that increase in power as you defeat them.
 
 Your goal is to defeat 5 enemies to "beat" the game.
-
-[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
--- bundle.js
+++ bundle.js
@@ -237,7 +237,6 @@ async function startGameLoop() {
 
     // then I want to wait until a user clicks them
     const choice = await waitForChoice();
-    console.log(choice);
     battleLogic(choice);
 
     // TODO refactor into a game logic function that changes action per choice
@@ -247,13 +246,17 @@ async function startGameLoop() {
         setTimeout(() => {
           toggleChoicesBox();
         }, 1000);
-        console.log("choice is fight, new function test");
         const animationClass = "animation-fight";
+
+        // if to check if character is alive before dealing damage
+        // TODO: should end combat if dead (not possible in current stage)
         if (character.health > 0) {
           triggerAnimation(mobImage, animationClass);
           activeMob.health -= myDamage;
           displayMobInfo(activeMob);
         }
+
+        // if to check if mob is dead to prevent damage dealt
         if (activeMob.health > 0) {
           setTimeout(() => {
             triggerAnimation(characterImage, animationClass);
@@ -262,14 +265,14 @@ async function startGameLoop() {
           }, 1000);
         }
       } else if (choice === "power") {
-        console.log("power attack chosen");
+        // TODO: add logic for this choice
       } else if (choice === "heal") {
-        console.log("heal chosen");
+        // TODO: add logic for this choice
       }
     }
   }
 
-  // TODO add function to get new mob, levelup feature
+  // TODO: add function to get new mob, levelup after dead mob
   setTimeout(() => {
     alert("you beat the alpha demo! great work!");
   }, 2000);
@@ -284,6 +287,8 @@ function toggleChoicesBox() {
   const container = document.getElementById("user-choices-container");
   container.style.display = container.style.display == "block" ? "none" : "block";
 }
+
+// awaiting promise from a button press
 function waitForChoice() {
   const fightButton = document.getElementById("fight");
   const powerButton = document.getElementById("power");
@@ -292,10 +297,11 @@ function waitForChoice() {
     fightButton.addEventListener("click", function () {
       resolve("fight");
     });
-    // TODO figure out what game actions I want to use
+    // TODO: which game actions I want to use, possibly change name per class selection
     powerButton.addEventListener("click", function () {
       resolve("power");
     });
+    // TODO: if using heal, need characters to have potions
     healButton.addEventListener("click", function () {
       resolve("heal");
     });
@@ -305,12 +311,10 @@ function waitForChoice() {
 // this function triggers animation by adding the class to the image elemnt
 function triggerAnimation(target, animationClass) {
   target.classList.add(animationClass);
-  console.log("animation class added");
   // timeout is set to same time as animation length
   setTimeout(() => {
     target.classList.remove(animationClass);
   }, 500);
-  console.log("animation class removed");
 }
 
 },{"./mobs/mobs":10,"./modules/chooseClass":11,"./modules/diplayCharacterInfo":12,"./modules/displayMobInfo":13,"./modules/enableCharacterInfoDisplays":14,"./modules/setActiveMob":15}],7:[function(require,module,exports){
